78652ee3bad6d3b33440041269718c7906a1b636
Eloam <y.breuil@outlook.com>
2021-07-04 15:10:50 +0200

    Amélioration espace commentaires

--- components/posts/leave-comment/leave-comment-component.php
+++ components/posts/leave-comment/leave-comment-component.php
@@ -14,7 +14,7 @@ class LeaveCommentComponent extends Component
         $this->context['commentFormArgs'] = array(
           'class_container' => 'rounded-lg bg-gray-800 text-white p-8 my-4 shadow-lg border border-gray-200',
           'class_form' => 'grid grid-cols-2 gap-4',
-          'class_submit' => 'inline-flex items-center px-4 py-2 text-base font-medium rounded-md shadow-sm text-white bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500',
+          'class_submit' => 'w-full px-4 py-2 text-base font-medium rounded-md shadow-sm text-white bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500',
         );
     }
 
